107fe9f2f96a78ee2635981032f48a67b27eaf1b
MobCop <84757503+MobCop@users.noreply.github.com>
2024-01-02 15:01:08 -0500

    initial setup

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+"use client"
+
+
+function Home() {
+
+  
+  return (
+    <div className="bg-white w-screem h-screen">
+      <div className="flex h-full w-full justify-center text-black text-[5vw] font-bold translate-y-[15vh]">
+      <h1>
+        Welcome to Split
+      </h1>
+      <h1 className="pl-[0.25vw]">
+        Easy
+      </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
